15218fca83c3d5bdbb98a199f649dfda9bae4b3e
Lorena Cueva <lorenacueva@gmail.com>
2022-06-10 15:14:14 -0700

    commit tree df2db208c2b5de37dd9ef7022fec4032420a63e9

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,7 @@
-// Code Keypad Component Here
+function Keypad(){
+    return (
+            <input type="password" placeholder="Password" onChange={() => console.log("Entering password...")}></input>
+    );
+}
+
+export default Keypad
